758552f7db7f6cb69d84ac65c6a42de40364cfb6
Artur Rushanyan <arturrushanyan@simplytechnologies.net>
2022-08-03 14:24:54 +0400

    Added new configuration module for db connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,17 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { User } from './user/user.model'
+import { DbconfigModule } from './dbconfig/dbconfig.module';
 
 
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [User],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
     UserModule,
+    DbconfigModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/dbconfig/dbconfig.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { User } from '../user/user.model'
+
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true
+          }),
+          TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+              type: 'postgres',
+              host: configService.get('DB_HOST'),
+              port: +configService.get<number>('DB_PORT'),
+              username: configService.get('DB_USERNAME'),
+              password: configService.get('DB_PASSWORD'),
+              database: configService.get('DB_NAME'),
+              entities: [User],
+              synchronize: true,
+            }),
+            inject: [ConfigService],
+          }),
+    ]
+})
+export class DbconfigModule {}
