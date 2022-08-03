1fc2b27ced9da90a22132ce1b04134cb350945e2
Artur Rushanyan <arturrushanyan@simplytechnologies.net>
2022-08-03 12:33:53 +0400

    Added "user" module and created "user" model.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User } from './user/user.model'
+
 
 
 @Module({
@@ -21,11 +23,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
-        entities: [],
+        entities: [User],
         synchronize: true,
       }),
       inject: [ConfigService],
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/user/user.model.ts
@@ -0,0 +1,70 @@
+import { IsEmail } from "class-validator";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+
+enum USER_TYPE {
+    MENTOR = 'mentor',
+    MENTEE = 'mentee'
+} 
+
+enum WORKING_FIELD {
+    IT = 'IT',
+    PM = 'Project Management',
+    DA = 'Data Analyst'
+}
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    surname: string;
+
+    @Column({
+        type: 'enum',
+        enum: USER_TYPE,
+        default: USER_TYPE.MENTOR
+    })
+    user_type: USER_TYPE
+
+    @Column()
+    position: string;
+
+    @Column({
+        type: 'enum',
+        enum: WORKING_FIELD,
+        default: WORKING_FIELD.IT
+    })
+    working_field: WORKING_FIELD;
+
+    @Column()
+    plans: string;
+
+    @Column({
+        unique: true
+    })
+    @IsEmail()
+    email: string;
+
+    @Column()
+    education: string;
+
+    @Column()
+    experience: string;
+
+    @Column()
+    about: string;
+
+    @Column()
+    password: string;
+
+    @CreateDateColumn()
+    createdAt: string;
+
+    @UpdateDateColumn()
+    updatedAt: string;
+}
