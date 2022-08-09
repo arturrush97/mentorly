5d57732f95845b13ed3edc6a330df2eeea335224
Artur Rushanyan <arturrushanyan@simplytechnologies.net>
2022-08-09 15:09:59 +0400

    Updated "Utils module" to get necessary keys for "JWT"  from ".env" file.

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -2,18 +2,25 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtUtilService } from './jwt.service';
 import { HashService } from './hash.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [JwtUtilService, HashService],
   exports: [JwtUtilService, HashService],
   imports: [
-    JwtModule.register({
-      secret: process.env.JWT_PRIVATE_KEY || 'top_secret',
-      signOptions: {
-        expiresIn: '24h'
-      }
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_PRIVATE_KEY') || 'top_secret',
+          signOptions: {
+            expiresIn: configService.get('TOKEN_EXPIRES_IN')
+          }
+        }
+      },
+      inject: [ConfigService]
     }),
   ]
-  
+
 })
-export class UtilsModule {}
+export class UtilsModule { }
